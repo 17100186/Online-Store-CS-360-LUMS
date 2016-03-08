c33b1a0ef979b662fbdf8f496c663676bd106d28
BullsEye <17100242@lums.edu.pk>
2016-03-08 17:47:13 +0000

    home page setup, sytyling and tabs

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+def new
+end
+
+def create
+  user = User.authenticate(params[:email],params[:password])
+  if user
+    session[:user_id] = user.id
+    redirect_to books_path, :notice => "Logged in!"
+  else
+    flash[:notice] = "Inavlid"
+    redirect_to log_in_path
+  end
+end
+
+def destroy
+  session[:user_id] = nil
+  redirect_to root_url, :notice => "Logged out!"
+end
+end
